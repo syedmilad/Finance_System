f0422628ee67f1cb3f7c84bb5b322bd93fa0b7fb
Milad-Aijazi <syedmilad245@gmail.com>
2025-03-23 01:51:33 +0500

    fix: runing padding, removing the * padding in index.css

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Welcome = () => {
   return (
-    <div className='border border-primary p-3 bg-red-300'>
-      asfasdf asdfsdffd
+    <div className="bg-red-100">
+      <div className='px-2 py-4'>
+        <h1>Welcome to the <br />
+          Finebank - Financial Management Dashboard</h1>
+        <p>This file is totally free for personal project & education purpose, for commercial license please contact us</p>
+      </div>
+      <div className='w-[200px] h-[3px] bg-[#878787] ' />
+      <div className='  '>
+        <h1>To download complete dashboard click below button. <br />
+          Don't worry, its totally free</h1>
+        <button>Finebank Dashboard</button>
+
+        <p>Download more free UI Resources here</p>
+      </div>
     </div>
   )
 }
