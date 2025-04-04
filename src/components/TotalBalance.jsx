c7ef0b1fd16d98fe96dcd65b642f557a39fe6489
Milad-Aijazi <syedmilad245@gmail.com>
2025-04-04 15:52:38 +0500

    updated code.

--- src/components/TotalBalance.jsx
+++ src/components/TotalBalance.jsx
@@ -25,7 +25,7 @@ const TotalBalance = () => {
         <div className="flex flex-col w-full h-full !overflow-x-hidden !overflow-y-hidden">
             {/* Header */}
             <div className='flex flex-row justify-start items-start'>
-                <span className='text-2xl text-[#878787] font-normal'>Total Balance</span>
+                <span className='text-2xl text-[#878787] font-normal mb-1'>Total Balance</span>
             </div>
             {/* Total Amount */}
             <div className='shadow-2xl bg-[#fff] rounded-[8px]  p-6'>
