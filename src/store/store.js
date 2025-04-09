3eee275853afb776033dea6898fee373a735f710
Milad-Aijazi <syedmilad245@gmail.com>
2025-04-09 12:49:31 +0500

    updated code.

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
-import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import {thunk} from "redux-thunk";
+import { applyMiddleware, compose, createStore } from "redux";
+import { thunk } from "redux-thunk";
 
 import balanceReducer from "../reducers/balanceReducer";
 
 const store = createStore(
   balanceReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
